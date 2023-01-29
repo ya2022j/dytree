de7a0c09e6fb75286308d3c6e451520804d44e38
Greenbirch2007 <min2020fr@gmail.com>
2023-01-29 11:15:05 +0900

    first commit

---
+++ static/flare-data.js
@@ -0,0 +1,79 @@
+var flare_data = {
+        "name": "flare",
+        "children": [
+      
+
+
+ 
+         
+         {
+          "name": "vis",
+          "children": [
+           {
+            "name": "axis",
+            "children": [
+             {"name": "Axes",
+        
+                "children":[
+                        {
+                "name":"1",
+                        }
+                ]
+        },
+             {"name": "Axis"},
+             {"name": "AxisGridLine"},
+             {"name": "AxisLabel"},
+             {"name": "CartesianAxes"}
+            ]
+       }
+
+
+]},{
+        "name": "vis",
+        "children": [
+         {
+          "name": "axis",
+          "children": [
+           {"name": "Axes",
+      
+              "children":[
+                      {
+              "name":"1",
+                      }
+              ]
+      },
+           {"name": "Axis"},
+           {"name": "AxisGridLine"},
+           {"name": "AxisLabel"},
+           {"name": "CartesianAxes"}
+          ]
+     }
+
+
+]},
+{"name":"sss"}
+
+,
+{"name":"demo-data.js"},
+{"name":"flare-data.js"},
+{
+        "name":"Golang",
+        "children":[
+                {"name":'d.pdf'}
+        ]
+        
+},
+{
+        "name":"Python",
+        "children":[
+                {
+                        "name":"d",
+                        "children":[
+                                {"name":'t.py'},
+                                {"name":"dd"}
+                        ]
+                }
+        ]
+},{"name":"ab","children":[{"name":'ac', "children":[{"name":1}]}]}
+
+]}
